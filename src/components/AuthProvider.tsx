4f58c65f511c85785ff9c72b7a1d0bfa7f45fb10
solar <nathf1005@naver.com>
2025-01-23 01:22:23 +0900

    add: AuthProvider 컴포넌트 추가 및 사용자 인증 로직 구현

---
+++ src/components/AuthProvider.tsx
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import { Collections, User } from '../type';
+import AuthContext from './AuthContext';
+import React from 'react';
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [initialized, setInitialized] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const [processingSignup, setProcessingSignup] = useState(false);
+  useEffect(() => {
+    const unsubsribe = auth().onUserChanged(async fbUser => {
+      if (fbUser != null) {
+        //login
+        setUser({
+          userId: fbUser.uid,
+          email: fbUser.email ?? '',
+          name: fbUser.displayName ?? '',
+        });
+      } else {
+        //logout
+        setUser(null);
+      }
+      setInitialized(true);
+    });
+    return () => {
+      unsubsribe();
+    };
+  }, []);
+
+  const signup = useCallback(
+    async (email: string, password: string, name: string) => {
+      setProcessingSignup(true);
+      try {
+        const { user: currentUser } =
+          await auth().createUserWithEmailAndPassword(email, password);
+        await currentUser.updateProfile({ displayName: name });
+        await firestore()
+          .collection(Collections.USER)
+          .doc(currentUser.uid)
+          .set({
+            userId: currentUser.uid,
+            email,
+            name,
+          });
+      } finally {
+        setProcessingSignup(false);
+      }
+    },
+    [],
+  );
+
+  const value = useMemo(() => {
+    return {
+      initialized,
+      user,
+      signup,
+      processingSignup,
+    };
+  }, [initialized, user, signup, processingSignup]);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
--- src/type.ts
+++ src/type.ts
@@ -7,3 +7,7 @@ export interface User {
   email: string;
   name: string;
 }
+
+export enum Collections {
+  USER = 'users',
+}
