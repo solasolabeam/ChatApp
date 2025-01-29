08c3fb9216d761a2d8aa27c7276f1c090267388a
solar <nathf1005@naver.com>
2025-01-30 02:56:00 +0900

    add: 채팅 화면에서 사용자 정보 표시 및 채팅 데이터 로드 기능 추가

--- src/ChatScreen/ChatScreen.tsx
+++ src/ChatScreen/ChatScreen.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Screen from '../components/Screen';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { RootStackParamList } from '../type';
 
 const ChatScreen = () => {
-  return <Screen title="채팅" />;
+  const { params } = useRoute<RouteProp<RootStackParamList, 'Chat'>>();
+  const { other } = params;
+  console.log('params', params);
+  return <Screen title={other.name} />;
 };
 
 export default ChatScreen;
---
+++ src/ChatScreen/useChat.ts
@@ -0,0 +1,61 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Chat, Collections, User } from '../type';
+import firestore from '@react-native-firebase/firestore';
+import _ from 'lodash';
+
+const getChatKey = (userIds: string[]) => {
+  return _.orderBy(userIds, userId => userId, 'asc');
+};
+
+const useChat = (userIds: string[]) => {
+  const [chat, setChat] = useState<Chat | null>(null);
+  const [loadingChat, setLoadingChat] = useState(false);
+
+  const loadChat = useCallback(async () => {
+    try {
+      setLoadingChat(true);
+      const chatSnapShot = await firestore()
+        .collection(Collections.CHATS)
+        .where('userIds', '==', getChatKey(userIds))
+        .get();
+
+      if (chatSnapShot.docs.length > 0) {
+        const doc = chatSnapShot.docs[0];
+        setChat({
+          id: doc.id,
+          userIds: doc.data().userIds as string[],
+          users: doc.data().userId as User[],
+        });
+        return;
+      }
+
+      const usersSnapShot = await firestore()
+        .collection(Collections.USERS)
+        .where('userid', 'in', userIds)
+        .get();
+      const users = usersSnapShot.docs.map(doc => doc.data() as User);
+      const data = {
+        userIds: getChatKey(userIds),
+        users,
+      };
+      const doc = await firestore().collection(Collections.CHATS).add(data);
+      setChat({
+        id: doc.id,
+        ...data,
+      });
+    } finally {
+      setLoadingChat(false);
+    }
+  }, [userIds]);
+
+  useEffect(() => {
+    loadChat();
+  }, [loadChat]);
+
+  return {
+    chat,
+    loadChat,
+  };
+};
+
+export default useChat;
