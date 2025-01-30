bed4088643a8685fdb6957561a13d99e518d603b
solar <nathf1005@naver.com>
2025-01-30 14:39:45 +0900

    add: 채팅 화면에 로딩 인디케이터 및 사용자 목록 표시 기능 추가

--- src/ChatScreen/ChatScreen.tsx
+++ src/ChatScreen/ChatScreen.tsx
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Screen from '../components/Screen';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../type';
 import useChat from './useChat';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 const ChatScreen = () => {
   const { params } = useRoute<RouteProp<RootStackParamList, 'Chat'>>();
   const { other, userIds } = params;
   const { loadingChat, chat } = useChat(userIds);
-  console.log('params', params);
-  return <Screen title={other.name} />;
+  console.log('chat.users', chat?.users);
+  const renderChat = useCallback(() => {
+    if (chat == null) {
+      return null;
+    }
+    return (
+      <View style={styles.chatContainer}>
+        <View style={styles.membersSection}>
+          <Text style={styles.membersTitleText}>대화상대</Text>
+          <FlatList
+            data={chat.users}
+            renderItem={({ item: user }) => (
+              <View style={styles.userProfile}>
+                <Text style={styles.userProfileText}>{user.name[0]}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    );
+  }, []);
+
+  return (
+    <Screen title={other.name}>
+      <View style={styles.container}>
+        {loadingChat ? (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator />
+          </View>
+        ) : (
+          renderChat()
+        )}
+      </View>
+    </Screen>
+  );
 };
 
 export default ChatScreen;
